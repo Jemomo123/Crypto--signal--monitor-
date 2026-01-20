a0e4437d66175bb01d1e0b55241babcfd572da10
Jemomo123 <blessedmosomi@gmail.com>
2026-01-20 23:13:20 +0300

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,21 +1,17 @@
 import ccxt
-import time
+import pandas as pd
 
-exchanges = {
-    "mexc": ccxt.mexc(),
-    "gate": ccxt.gate()
-}
 
-def fetch_ohlcv(exchange_name, symbol, timeframe, limit=200):
-    exchange = exchanges.get(exchange_name)
+def fetch_ohlcv(exchange_id, symbol, timeframe, limit=200):
+    exchange_class = getattr(ccxt, exchange_id)
+    exchange = exchange_class({"enableRateLimit": True})
 
-    if not exchange:
-        return []
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
 
-    try:
-        exchange.load_markets()
-        data = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-        time.sleep(exchange.rateLimit / 1000)
-        return data
-    except Exception as e:
-        return []
+    df = pd.DataFrame(
+        ohlcv,
+        columns=["timestamp", "open", "high", "low", "close", "volume"]
+    )
+
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+    return df
